2f14b618f6aa7ddbd73caecde01de2f0b6fd3bd6
Ubuntu <ubuntu@ip-172-31-19-236.eu-west-2.compute.internal>
2021-06-20 18:15:02 +0000

    changes

--- index.py
+++ index.py
@@ -40,7 +40,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=options)
+#driver = webdriver.Chrome("./chromedriver", options=options)
 
 users = db.mydb["users"]
 tweets_mongo = db.mydb["tweets"]
@@ -422,4 +422,4 @@ def dashboard():
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0', port=5000)
